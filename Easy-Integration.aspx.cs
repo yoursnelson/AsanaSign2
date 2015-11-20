254ccf027c95e30e7a4c8b40bcd928e0f7226104
yoursnelson <yoursnelson@hotmail.com>
2015-11-20 18:35:49 +0800

    Added Load2

--- Easy-Integration.aspx.cs
+++ Easy-Integration.aspx.cs
@@ -16,6 +16,18 @@ partial class Easy_Integration : System.Web.UI.Page
         }
     }
 
+	protected void Page_Load2(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //Get PDF as byte array from file (or database, browser upload, remote storage, etc)
+            byte[] pdfData = System.IO.File.ReadAllBytes(Server.MapPath(@"pdfs/RadPdfSampleForm.pdf"));
+
+            //Load PDF byte array into RAD PDF
+            this.PdfWebControl1.CreateDocument("Document Name", pdfData);
+        }
+    }
+	
     protected void Saved(object sender, DocumentSavedEventArgs e)
     {
         //Check what raised the Saved event
